d8e58ff34efda0254c35886e20c0bc1054bb2aa9
blackmamba17 <scuolatorrisi@gmail.com>
2025-03-03 23:27:22 +0100

    added Ulist component to footer to make it more dinamic and readable

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,17 @@
-import React from "react";
 import { Ulist } from "./Ulist";
-import { NAVBAR_LIST_ICONS_AND_LINKS } from "../utils/constants";
+import {
+  NAVBAR_LIST_ICONS_AND_LINKS,
+  NAVBAR_LIST_ITEMS,
+} from "../utils/constants";
 
 export function Footer() {
   return (
     <footer className="section footer">
-      <ul className="footer-links">
-        <li>
-          <a href="#home" className="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" className="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" className="footer-link">
-            featured
-          </a>
-        </li>
-      </ul>
-
+      <Ulist
+        UlClassName="footer-links"
+        hyperLinkClassName="footer-link"
+        arrayOfHyperLinkItems={NAVBAR_LIST_ITEMS}
+      />
       <Ulist
         UlClassName="footer-icons"
         hyperLinkClassName="nav-icon"
